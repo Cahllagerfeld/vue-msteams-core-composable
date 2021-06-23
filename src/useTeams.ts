c7b0f0f9c0f72cd91c5e658234c96ba67687b268
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-06-23 20:12:26 +0200

    chore: initial commit

---
+++ src/useTeams.ts
@@ -0,0 +1,42 @@
+import * as microsoftTeams from "@microsoft/teams-js";
+import { Ref, ref, watchEffect } from "vue";
+
+export interface useTeamsResult {
+  isInTeams: Ref<boolean>;
+  context: Ref<microsoftTeams.Context | undefined>;
+}
+
+export function checkTeams(): boolean {
+  const microsoftTeamsLib = microsoftTeams || window["microsoftTeams"];
+
+  if (!microsoftTeamsLib) {
+    return false;
+  }
+
+  if (
+    (window.parent === window.self && (window as any).nativeInterface) ||
+    window.name === "embedded-page-container" ||
+    window.name === "extension-tab-frame"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+export default function useTeams(): useTeamsResult {
+  const context = ref<microsoftTeams.Context>();
+  const isInTeams = ref<boolean>(false);
+  watchEffect(() => {
+    isInTeams.value = checkTeams();
+    if (isInTeams) {
+      microsoftTeams.initialize(() => {
+        microsoftTeams.getContext((msTeamsContext: microsoftTeams.Context) => {
+          context.value = msTeamsContext;
+        });
+      });
+    } else {
+      microsoftTeams.initialize();
+    }
+  });
+  return { isInTeams, context };
+}
